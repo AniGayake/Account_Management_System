180ff6ceb14ea52ac15b26d04fc0bb2137ec751a
Aniruddha <anigayake101@gmail.com>
2022-04-12 09:42:10 +0530

    Entity classes - added required variables and methods

--- src/main/java/com/barclays/accountmanagement/entity/Users.java
+++ src/main/java/com/barclays/accountmanagement/entity/Users.java
@@ -1,5 +1,54 @@
 package com.barclays.accountmanagement.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Users")
 public class Users {
+	@Id
+	@NotNull
+	public long customerId;
+	public String password;
+	//public int roleId;
+	
+	public Users() {
+		
+	}
+	
+	
+	public Users(@NotNull long customerId, String password, int roleId) {
+		super();
+		this.customerId = customerId;
+		this.password = password;
+		//this.roleId = roleId;
+	}
+
+
+
+	public long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+//	public int getRoleId() {
+//		return roleId;
+//	}
+//	public void setRoleId(int roleId) {
+//		this.roleId = roleId;
+//	}
+//	
+	
+	
 
 }
+
