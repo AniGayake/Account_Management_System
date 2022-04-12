f08a050a2e7d1b745ad3e2ce191371dc89158c53
Aniruddha <anigayake101@gmail.com>
2022-04-12 14:04:48 +0530

    Credit and Debit transaction added and working

--- src/main/java/com/barclays/accountmanagement/AccountManagementSystemApplication.java
+++ src/main/java/com/barclays/accountmanagement/AccountManagementSystemApplication.java
@@ -1,13 +1,23 @@
 package com.barclays.accountmanagement;
 
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.barclays.accountmanagement.entity.Transaction;
+
+import net.bytebuddy.utility.RandomString;
+
 @SpringBootApplication
 public class AccountManagementSystemApplication {
-
+	
 	public static void main(String[] args) {
+	
 		SpringApplication.run(AccountManagementSystemApplication.class, args);
+		
+		
 	}
 
+	
 }
